ca2820ab744e13a807220853c85cf65cc7e9784e
UTKARSH-TYAGI-1247 <Utkarsh.22B0131012@abes.ac.in>
2024-10-21 13:14:35 +0530

    day8

---
+++ day8/p5.js
@@ -0,0 +1,16 @@
+async function orderfood(name, time) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(name + "Prepared")
+        },time);
+    });
+}
+
+async function restaurant() {
+    console.log("Task Started");
+    let pizza = await orderfood("Pizza" , 1000);
+    console.log(pizza);
+    const burger= await orderfood("Burger", 2000);
+    console.log(burger);
+}
+restaurant();
