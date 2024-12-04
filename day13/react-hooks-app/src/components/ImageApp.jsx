708052c8bbf12a1c8df89be8c289134b38d22ec3
UTKARSH-TYAGI-1247 <Utkarsh.22B0131012@abes.ac.in>
2024-12-04 12:45:36 +0530

    akd

--- day13/react-hooks-app/src/components/ImageApp.jsx
+++ day13/react-hooks-app/src/components/ImageApp.jsx
@@ -1,24 +1,35 @@
-import React, { useState } from 'react'
+import React from 'react'
 import "./ImageApp.css"
-import pic from "../Images/pic.png";
+import pic from '../images/ironman.jpg'
 const ImageApp = () => {
-    const [width,setWidth]=useState(200);
-    const[height,setHeight]=useState(200);
-    const[rotation,setRotation]=useState(0);
-  return (
+    const [width, setWidth] = React.useState(200);
+    const [height, setHeight] = React.useState(200);
+    const [rotation, setRotation]=React.useState(0);
+    return (
     <div className='disp'>
-      <div className="image">
-        <img
-        src={pic}
+        <div className='image'>
+        <img src={pic} 
         style={{
             width: width,
             height: height,
-            transform: rotate(${rotation}deg),
+            transform: `rotate(${rotation}deg)`,
         }}
-        />
-      </div>
-    </div>
-  );
-};
+        />  
+        
+        </div>
+        <div className='panel'>
+          <button onClick={()=>{setWidth(width+10)}}>Increase Width</button>
+          <button onClick={()=>{setWidth(width-10)}}>Decrease Width</button>
 
-export default ImageApp;
+          <button onClick={()=>{setHeight(height+10)}}>Increase Height</button>
+          <button onClick={()=>{setHeight(height-10)}}>Decrease Height</button>
+
+          <button onClick={()=>{setRotation(rotation+10)}}>Rotate ClockWise</button>
+          <button onClick={()=>{setRotation(rotation-10)}}>Rotate Anti-ClockWise</button>
+
+        </div>
+        </div>
+  )
+}
+
+export default ImageApp
